0fe9895525894c163a3257297b5950345d7e537e
Adam Robinson <adam.robinson@phizzard.com>
2019-05-20 19:14:06 +0200

    fix: fixed breaking test

--- app/src/test/kotlin/com/phizzard/es/TestUtils.kt
+++ app/src/test/kotlin/com/phizzard/es/TestUtils.kt
@@ -20,41 +20,8 @@ const val SAMPLE_ADDRESS = """
 const val SAMPLE_ORDER = """
 {
   "id": 123467890,
+  "approvalCode": "666",
   "buyerContact": {
-    "address": {
-      "id": 123467890,
-      "city": "Berlin",
-      "country": "Germany",
-      "federalState": "Berlin",
-      "latitude": 52.431181,
-      "longitude": 13.538744,
-      "street": "Am Studio",
-      "streetNumber": "2a",
-      "zipCode": "12489"
-    },
-    "email": "michael.mente@phizzard.com",
-    "firstName": "Michael",
-    "lastName": "Mente",
-    "phoneNumber": "49123456789910"
-  },
-  "deliveryContact": {
-    "address": {
-      "id": 123467890,
-      "city": "Berlin",
-      "country": "Germany",
-      "federalState": "Berlin",
-      "latitude": 52.431181,
-      "longitude": 13.538744,
-      "street": "Am Studio",
-      "streetNumber": "2a",
-      "zipCode": "12489"
-    },
-    "email": "michael.mente@phizzard.com",
-    "firstName": "Michael",
-    "lastName": "Mente",
-    "phoneNumber": "49123456789910"
-  },
-  "invoiceContact": {
     "address": {
       "id": 123467890,
       "city": "Berlin",
@@ -73,85 +40,20 @@ const val SAMPLE_ORDER = """
   },
   "orderItems": [
     {
-      "id": 123467890,
-      "brandName": "string",
-      "productDetails": [
-        {
-          "id": 123467890,
-          "productAttributes": [
-            {
-              "group": "DIMENSIONS",
-              "name": "COLOR",
-              "value": "peuce"
-            }
-          ]
-        }
-      ],
-      "productVariantDetails": [
-        {
-          "currency": "EUR",
-          "priceType": "sale",
-          "productVariantId": 123467890,
-          "productVariantAttributes": [
-            {
-              "group": "DIMENSIONS",
-              "name": "COLOR",
-              "value": "peuce"
-            }
-          ],
-          "storeDetails": {
-            "contactInfo": {
-              "address": {
-                "id": 123467890,
-                "city": "Berlin",
-                "country": "Germany",
-                "federalState": "Berlin",
-                "latitude": 52.431181,
-                "longitude": 13.538744,
-                "street": "Am Studio",
-                "streetNumber": "2a",
-                "zipCode": "12489"
-              },
-              "email": "michael.mente@phizzard.com",
-              "firstName": "Michael",
-              "lastName": "Mente",
-              "phoneNumber": "49123456789910"
-            },
-            "id": 123467890,
-            "name": "FootLocker",
-            "type": "Sporting Goods"
-          }
-        }
-      ],
-      "quantity": 2,
-      "supplierName": "kjus",
-      "value": 19.95
+      "articleDescription": "Ladies Kjus T-shirt",
+      "currency": "CHF",
+      "gtin": "07612997688944",
+      "grossPrice": 64.07,
+      "netPrice": 69.0,
+      "quantity": 1,
+      "shop": {
+        "name": "Pais",
+        "identifier": "200742"
+      },
+      "supplierName": "kjus"
     }
   ],
-  "status": "PROCESSING",
-  "storeDetails": {
-    "contactInfo": {
-      "address": {
-        "id": 123467890,
-        "city": "Berlin",
-        "country": "Germany",
-        "federalState": "Berlin",
-        "latitude": 52.431181,
-        "longitude": 13.538744,
-        "street": "Am Studio",
-        "streetNumber": "2a",
-        "zipCode": "12489"
-      },
-      "email": "michael.mente@phizzard.com",
-      "firstName": "Michael",
-      "lastName": "Mente",
-      "phoneNumber": "49123456789910"
-    },
-    "id": 123467890,
-    "name": "FootLocker",
-    "type": "Sporting Goods"
-  },
-  "uuid": "AF504EC2-4005-11E9-B16F-2436AB45578C"
+  "status": "PROCESSING"
 }
 """
 
